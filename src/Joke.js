9cf9e5c02bd244260672b9be7ab8ecdffbf354e4
basarasa <rasa.bertulyte@gmail.com>
2024-07-26 17:23:50 +1000

    fetch data from API

--- src/Joke.js
+++ src/Joke.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Jokes = () => {
+    const [joke, setJoke] = useState([]);
+    const apiLink = "https://icanhazdadjoke.com/";
+    const fetchData = async () => {
+        const res = await axios.get(`${apiLink}`, { headers: { Accept: "application/json" } });
+        console.log(res.data);
+        console.log(res.data.joke)
+    }
 return (
    <div>
       <h1>Jokes:</h1>
