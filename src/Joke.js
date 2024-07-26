634dae77d3ccc90878a6c33457a2e400f6a0a40f
basarasa <rasa.bertulyte@gmail.com>
2024-07-26 17:29:26 +1000

    The fetchData function updates the state with a new joke from the API, and a button calls this function to load a new joke each time it’s clicked.

--- src/Joke.js
+++ src/Joke.js
@@ -3,16 +3,27 @@ import axios from 'axios';
 
 const Jokes = () => {
     const [joke, setJoke] = useState([]);
+
     const apiLink = "https://icanhazdadjoke.com/";
+
     const fetchData = async () => {
         const res = await axios.get(`${apiLink}`, { headers: { Accept: "application/json" } });
         console.log(res.data);
         console.log(res.data.joke)
     }
 return (
-   <div>
-      <h1>Jokes:</h1>
-</div>
-);
+  <div className="joke__">
+            <h1>Jokes:</h1>
+               {/*Fetch data from url*/}
+                <button onClick={fetchData}>Give me a Dad joke</button>
+
+                     {/*data from api goes here*/}
+                    {joke && joke.map((j, index) => (
+                         <div className="data" key={index}>
+                          {j}
+                         </div>
+                     ))}
+        </div>
+    )
 }
 export default Jokes;
